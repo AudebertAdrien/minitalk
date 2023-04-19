662656cb6f931782ec1c16093a995415d24a12e9
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-04-19 15:43:56 +0200

    clean code

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:51:05 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/04/18 17:44:41 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:42:18 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,52 +24,33 @@ int	send_bits(int pid, unsigned char c)
 	while (i < 8)
 	{
 		res = (c << i) & mask;	
-		printf("res %d\n", res);
-		printf("i %d:\n", i);
-		
 		if (res)
-		{
-			printf("1");
 			kill(pid, SIGUSR1);
-		}
 		else
-		{
-			printf("0");	
 			kill(pid, SIGUSR2);
-		}
 		usleep(1000);
-		printf("\n////////\n");	
 		i++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	int		pid;
+	char	*str;
+	int		i;
 
-	if (argc == 2)
+	i = 0;
+	if (argc == 3)
 	{
+		str = argv[2];
 		pid = atoi(argv[1]);
-		printf("pid : %d\n", pid);
-		send_bits(pid, 'a');
-		usleep(1000);
-		send_bits(pid, 8);
+		while (str[i])
+		{
+			send_bits(pid, str[i]);
+			usleep(1000);
+			i++;
+		}
 	}
+	printf("error : number of args\n");
 	return (0);
 }
-
-/*
-void	test_bits(unsigned char c)
-{
-	printf("test_bits :\n");	
-	int n = 0;
-	int a = 128;
-	int b = 0;
-
-	b = a | (1 << 0); 
-	// a = 10000000 | 00001000 (= 10001000)
-	//b |= (1 << 2);
-	//b |= 16;
-	send_bits(1, b);
-}
-*/
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:51:05 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/04/18 17:44:57 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/04/19 15:43:02 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,21 @@
 
 volatile int	bit_nb = 0;
 
-int	print_bits(unsigned char c) {
-	printf("print_bits\n");
-	int	mask;
-	int	i;
-	int	res;
-
-	i = 0;
-	res = 0;
-	mask = 128;
-	while (i < 8)
-	{
-		res = (c << i) & mask;	
-		if (res)
-			printf("1");
-		else
-			printf("0");	
-		i++;
-	}
-	printf("\n/////////\n");	
-}
-
 void	sigint_handler(int signal)
 {
 	static char	c;
 	
 	if (!c)
 		c = 0;	
-
-	printf("bit_nb : %d", bit_nb);
 	if (signal == SIGUSR1)
-	{
 		c = c | (128 >> bit_nb);
-		printf("1");
-	}
 	if (signal == SIGUSR2)
-	{
-		//c = c & ~(1 << bit_nb);
-		printf("0");
-	} 
-	printf("\n");
+		c = c & ~(128 >> bit_nb);
 	bit_nb += 1;
-
+	printf("\n");
 	if (bit_nb == 8)
 	{
-		printf("\n////////\n");
-		print_bits(c);
-		printf("c = %c\n", c);
+		printf("%c", c);
 		bit_nb = 0;
 		c = 0;
 	}
@@ -68,15 +36,10 @@ void	sigint_handler(int signal)
 
 void	set_signal_action(void)
 {
-	printf("set_signal_action\n"); 
-
 	struct	sigaction act;
-	int	res;
 	
 	bzero(&act , sizeof(act));
-	
 	act.sa_handler = &sigint_handler;
-
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
 }
@@ -84,9 +47,7 @@ void	set_signal_action(void)
 int	main(void)
 {
 	printf("getpid : %d\n", getpid());
-
 	set_signal_action();
-
 	while (1)
 	{
 		sleep(1);
