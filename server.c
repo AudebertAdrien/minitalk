c4babf79d386e4433dd4a3231d21bd8b88c97538
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-04-26 15:31:06 +0200

    casse les couilles

--- server.c
+++ server.c
@@ -6,11 +6,33 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:51:05 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/04/26 14:58:23 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/04/26 15:29:50 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+
+void print_bits(unsigned int c, int bits_nb)
+{
+	int	i;
+	int	res;
+	int	mask;
+
+	i = 0;
+	res = 0;
+	mask = 1;
+	while (i < bits_nb)
+	{
+		res = (c >> i) & mask;	
+		if (res)
+			printf("1");
+		else
+			printf("0");
+		i++;
+	}
+	printf("\n");
+}
 
 void	bits_to_int(int signal, int *x, int byte)
 {
@@ -21,36 +43,41 @@ void	bits_to_int(int signal, int *x, int byte)
 	if (signal == SIGUSR2)
 		*x &= ~(1 << bit_nb);
 	bit_nb += 1;
+	if (byte == 32)
+		print_bits(*x, 32);
+	if (byte == 8)
+		print_bits(*x, 8);
 	if (bit_nb == byte)
 	{
 		if (byte == 32)
-			printf("byte 32 : %d\n", *x);
+			printf("\nbyte 32 : %d\n", *x);
 		if (byte == 8)
-			printf("byte 8 : %d\n", *x);
+			printf("\nbyte 8 : %d\n", *x);
 		bit_nb = 0;
 	}
 }
 
 void	sigint_handler(int signal)
 {
-	static char	*str;
-	static int	len = 0;
+	//static char	*str;
 	static int	sys_call = 0;
-	static int	i = 0;
+	static int	len = 0;
 	static int	c = 0;
 	
 	if (sys_call == 32)
 	{
 		//str = ft_calloc((len + 1),sizeof(char));
 		printf("len : %d\n",len);
-		printf("sizeof : %ld\n", sizeof(str));
+		//printf("sizeof : %ld\n", sizeof(str));
 	}
 	
 	if (sys_call < 32)
 		bits_to_int(signal, &len, 32);
-	else
+
+	if (sys_call > 32)
 	{
 		bits_to_int(signal, &c, 8);
+
 		if (sys_call == (32 + len))
 		{
 			printf("END!!!! %d\n", 32 + len);
@@ -58,9 +85,11 @@ void	sigint_handler(int signal)
 			len = 0;
 			c = 0;
 			len = 0;
+			/*
 			ft_bzero(str);
 			free(str);
 			str = NULL;
+			*/
 		}
 	}
 
